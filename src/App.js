5ea69791d98df1c34ca1e2421695d673b54499ad
Sam <samuelguttormson@gmail.com>
2022-01-29 15:06:24 -0600

    initial commit, literally nothing here

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={require('./kendrick.png')} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bad Cat
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          good cat
+        </button>
+        <button>
+          bad cat
+        </button>
       </header>
     </div>
   );
