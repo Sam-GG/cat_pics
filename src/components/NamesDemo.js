44e67054f1999305891c13b717d12867bd51d7c4
Sam <samuelguttormson@gmail.com>
2022-02-15 20:25:38 -0600

    improving word cloud aesthetic

--- src/components/NamesDemo.js
+++ src/components/NamesDemo.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactWordcloud from 'react-wordcloud';
 const words = [
-    {text: 'John', value: 10},
-    {text: 'Paul', value: 5},
-    {text: 'George', value: 25},
-    {text: 'Ringo', value: 40},
+    {text: 'John', value: 30},
+    {text: 'Paul', value: 25},
+    {text: 'George', value: 50},
+    {text: 'Ringo', value: 70},
 ];
+
+const options = {
+    rotations: 2,
+    rotationAngles: [-20, 20],
+    fontFamily: "impact",
+    fontSizes: [14, 50],
+    scale: "linear"
+};
+
 export const NamesDemo = () => {
-    return (<ReactWordcloud words={words} />);
+    return (
+        <div className="names-demo">
+            <ReactWordcloud words={words} options={options} />
+        </div>
+    );
 }
 
 export default NamesDemo;
